2d321ee0441285f99f522a7181e1105f4e4702c8
AjayGodara2417 <agodara@gmail.com>
2026-04-01 15:07:50 +0530

    Separate questions file for main assessment

---
+++ src/constants/extraQuestions.ts
@@ -0,0 +1,22 @@
+export const personalQuestions = [
+  {
+    id: 1,
+    question: "Any illness in recent time?",
+    options: ["No", "Mild", "Moderate", "Severe"],
+  },
+  {
+    id: 2,
+    question: "Any disturbing thoughts in your mind?",
+    options: ["None", "Sometimes", "Frequent", "Constant"],
+  },
+  {
+    id: 3,
+    question: "Any financial stress?",
+    options: ["No stress", "Low", "Moderate", "High"],
+  },
+  {
+    id: 4,
+    question: "Your mood today?",
+    options: ["Happy", "Neutral", "Low", "Very Low"],
+  },
+];
